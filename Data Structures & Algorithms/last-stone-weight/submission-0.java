a6ce665684416daae829c6dee681eb1cbdf6f6e1
NigelPn <nigelpoonweijun@gmail.com>
2026-06-23 00:40:24 +0800

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int stone : stones) maxHeap.offer(stone);
+    
+
+        while (maxHeap.size() > 1){
+            maxHeap.offer(maxHeap.poll()-maxHeap.poll());
+        }
+
+        return maxHeap.peek();
+    }
+}
